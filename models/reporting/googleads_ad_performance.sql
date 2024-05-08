5b568cfb5ddf4d76f3bd7051da3871cccb64fe58
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-05-08 16:33:21 -0400

    Update googleads_ad_performance.sql
    signups + consultation payments

--- models/reporting/googleads_ad_performance.sql
+++ models/reporting/googleads_ad_performance.sql
@@ -2,6 +2,8 @@
     alias = target.database + '_googleads_ad_performance'
 )}}
 
+{% set date_granularity_list = ['day', 'week', 'month', 'quarter', 'year'] %}
+    
 SELECT
 account_id,
 ad_id,
@@ -18,5 +20,16 @@ impressions,
 clicks,
 conversions as purchases,
 conversions_value as revenue
-
 FROM {{ ref('googleads_performance_by_ad') }}
+LEFT JOIN 
+    ({%- for date_granularity in date_granularity_list %}
+        SELECT '{{date_granularity}}' as date_granularity,
+            {{date_granularity}} as date,
+            customer_id as account_id, id as campaign_id,
+            COALESCE(SUM(CASE WHEN conversion_action_category = 'SIGNUP' THEN conversions END),0) as signups,
+            COALESCE(SUM(CASE WHEN conversion_action_category = 'PURCHASE' THEN conversions END),0) as consultation_payment
+        FROM {{ source('googleads_raw','ad_convtype_performance_report') }}
+        GROUP BY 1,2,3,4
+        {% if not loop.last %}UNION ALL
+        {% endif %}
+    {% endfor %})) USING(date_granularity, date, account_id, campaign_id)
