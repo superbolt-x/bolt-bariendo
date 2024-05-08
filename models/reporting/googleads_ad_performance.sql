1d0aa68b8e68ca83fb075ef087ca58f591744b8d
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-05-08 17:26:05 -0400

    Update googleads_ad_performance.sql

--- models/reporting/googleads_ad_performance.sql
+++ models/reporting/googleads_ad_performance.sql
@@ -19,17 +19,48 @@ spend,
 impressions,
 clicks,
 conversions as purchases,
-conversions_value as revenue
+conversions_value as revenue,
+signups, 
+consultation_payment
 FROM {{ ref('googleads_performance_by_ad') }}
 LEFT JOIN 
-    ({%- for date_granularity in date_granularity_list %}
-        SELECT '{{date_granularity}}' as date_granularity,
-            {{date_granularity}} as date,
-            customer_id as account_id, id as campaign_id,
-            COALESCE(SUM(CASE WHEN conversion_action_category = 'SIGNUP' THEN conversions END),0) as signups,
-            COALESCE(SUM(CASE WHEN conversion_action_category = 'PURCHASE' THEN conversions END),0) as consultation_payment
-        FROM {{ source('googleads_raw','ad_convtype_performance_report') }}
-        GROUP BY 1,2,3,4
-        {% if not loop.last %}UNION ALL
-        {% endif %}
-    {% endfor %}) USING(date_granularity, date, account_id, campaign_id)
+    (SELECT 'day' as date_granularity,
+        DATE_TRUNC('day',date::date) as date,
+        customer_id as account_id, id as campaign_id,
+        COALESCE(SUM(CASE WHEN conversion_action_category = 'SIGNUP' THEN conversions END),0) as signups,
+        COALESCE(SUM(CASE WHEN conversion_action_category = 'PURCHASE' THEN conversions END),0) as consultation_payment
+    FROM {{ source('googleads_raw','ad_convtype_performance_report') }}
+    GROUP BY 1,2,3,4
+    UNION ALL
+    SELECT 'week' as date_granularity,
+        DATE_TRUNC('week',date::date) as date,
+        customer_id as account_id, id as campaign_id,
+        COALESCE(SUM(CASE WHEN conversion_action_category = 'SIGNUP' THEN conversions END),0) as signups,
+        COALESCE(SUM(CASE WHEN conversion_action_category = 'PURCHASE' THEN conversions END),0) as consultation_payment
+    FROM {{ source('googleads_raw','ad_convtype_performance_report') }}
+    GROUP BY 1,2,3,4
+    UNION ALL
+    SELECT 'month' as date_granularity,
+        DATE_TRUNC('month',date::date) as date,
+        customer_id as account_id, id as campaign_id,
+        COALESCE(SUM(CASE WHEN conversion_action_category = 'SIGNUP' THEN conversions END),0) as signups,
+        COALESCE(SUM(CASE WHEN conversion_action_category = 'PURCHASE' THEN conversions END),0) as consultation_payment
+    FROM {{ source('googleads_raw','ad_convtype_performance_report') }}
+    GROUP BY 1,2,3,4
+    UNION ALL
+    SELECT 'quarter' as date_granularity,
+        DATE_TRUNC('quarter',date::date) as date,
+        customer_id as account_id, id as campaign_id,
+        COALESCE(SUM(CASE WHEN conversion_action_category = 'SIGNUP' THEN conversions END),0) as signups,
+        COALESCE(SUM(CASE WHEN conversion_action_category = 'PURCHASE' THEN conversions END),0) as consultation_payment
+    FROM {{ source('googleads_raw','ad_convtype_performance_report') }}
+    GROUP BY 1,2,3,4
+    UNION ALL
+    SELECT 'year' as date_granularity,
+        DATE_TRUNC('year',date::date) as date,
+        customer_id as account_id, id as campaign_id,
+        COALESCE(SUM(CASE WHEN conversion_action_category = 'SIGNUP' THEN conversions END),0) as signups,
+        COALESCE(SUM(CASE WHEN conversion_action_category = 'PURCHASE' THEN conversions END),0) as consultation_payment
+    FROM {{ source('googleads_raw','ad_convtype_performance_report') }}
+    GROUP BY 1,2,3,4
+    ) USING(date_granularity, date, account_id, campaign_id)
