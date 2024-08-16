c5fab48c1c9fe3b089ab4dc028800ebebf11359b
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-08-16 16:14:04 -0400

    Update googleads_ad_performance.sql

--- models/reporting/googleads_ad_performance.sql
+++ models/reporting/googleads_ad_performance.sql
@@ -27,40 +27,40 @@ LEFT JOIN
     (SELECT 'day' as date_granularity,
         DATE_TRUNC('day',date::date) as date,
         customer_id as account_id, campaign_id, campaign_name, ad_group_name, ad_group_id, ad_id, 
-        COALESCE(SUM(CASE WHEN conversion_action_category = 'SIGNUP' THEN conversions END),0) as signups,
-        COALESCE(SUM(CASE WHEN conversion_action_category = 'PURCHASE' THEN conversions END),0) as consultation_payment
+        COALESCE(SUM(CASE WHEN conversion_action_name ~* 'signup' THEN conversions END),0) as signups,
+        COALESCE(SUM(CASE WHEN conversion_action_name ~* 'Consultation Payment' THEN conversions END),0) as consultation_payment
     FROM {{ source('googleads_raw','ad_convtype_performance_report') }}
     GROUP BY 1,2,3,4,5,6,7,8
     UNION ALL
     SELECT 'week' as date_granularity,
         DATE_TRUNC('week',date::date) as date,
         customer_id as account_id, campaign_id, campaign_name, ad_group_name, ad_group_id, ad_id, 
-        COALESCE(SUM(CASE WHEN conversion_action_category = 'SIGNUP' THEN conversions END),0) as signups,
-        COALESCE(SUM(CASE WHEN conversion_action_category = 'PURCHASE' THEN conversions END),0) as consultation_payment
+        COALESCE(SUM(CASE WHEN conversion_action_name ~* 'signup' THEN conversions END),0) as signups,
+        COALESCE(SUM(CASE WHEN conversion_action_name ~* 'Consultation Payment' THEN conversions END),0) as consultation_payment
     FROM {{ source('googleads_raw','ad_convtype_performance_report') }}
     GROUP BY 1,2,3,4,5,6,7,8
     UNION ALL
     SELECT 'month' as date_granularity,
         DATE_TRUNC('month',date::date) as date,
         customer_id as account_id, campaign_id, campaign_name, ad_group_name, ad_group_id, ad_id, 
-        COALESCE(SUM(CASE WHEN conversion_action_category = 'SIGNUP' THEN conversions END),0) as signups,
-        COALESCE(SUM(CASE WHEN conversion_action_category = 'PURCHASE' THEN conversions END),0) as consultation_payment
+        COALESCE(SUM(CASE WHEN conversion_action_name ~* 'signup' THEN conversions END),0) as signups,
+        COALESCE(SUM(CASE WHEN conversion_action_name ~* 'Consultation Payment' THEN conversions END),0) as consultation_payment
     FROM {{ source('googleads_raw','ad_convtype_performance_report') }}
     GROUP BY 1,2,3,4,5,6,7,8
     UNION ALL
     SELECT 'quarter' as date_granularity,
         DATE_TRUNC('quarter',date::date) as date,
         customer_id as account_id, campaign_id, campaign_name, ad_group_name, ad_group_id, ad_id, 
-        COALESCE(SUM(CASE WHEN conversion_action_category = 'SIGNUP' THEN conversions END),0) as signups,
-        COALESCE(SUM(CASE WHEN conversion_action_category = 'PURCHASE' THEN conversions END),0) as consultation_payment
+        COALESCE(SUM(CASE WHEN conversion_action_name ~* 'signup' THEN conversions END),0) as signups,
+        COALESCE(SUM(CASE WHEN conversion_action_name ~* 'Consultation Payment' THEN conversions END),0) as consultation_payment
     FROM {{ source('googleads_raw','ad_convtype_performance_report') }}
     GROUP BY 1,2,3,4,5,6,7,8
     UNION ALL
     SELECT 'year' as date_granularity,
         DATE_TRUNC('year',date::date) as date,
         customer_id as account_id, campaign_id, campaign_name, ad_group_name, ad_group_id, ad_id, 
-        COALESCE(SUM(CASE WHEN conversion_action_category = 'SIGNUP' THEN conversions END),0) as signups,
-        COALESCE(SUM(CASE WHEN conversion_action_category = 'PURCHASE' THEN conversions END),0) as consultation_payment
+        COALESCE(SUM(CASE WHEN conversion_action_name ~* 'signup' THEN conversions END),0) as signups,
+        COALESCE(SUM(CASE WHEN conversion_action_name ~* 'Consultation Payment' THEN conversions END),0) as consultation_payment
     FROM {{ source('googleads_raw','ad_convtype_performance_report') }}
     GROUP BY 1,2,3,4,5,6,7,8
     ) USING(date_granularity, date, account_id, campaign_id, campaign_name, ad_group_name, ad_group_id, ad_id)
