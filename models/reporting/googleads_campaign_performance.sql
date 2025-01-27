cd83a88eb8fd2062578a02534de9a8f2f33d46f1
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-01-27 14:41:45 -0600

    Update googleads_campaign_performance.sql
    signups & consultation_payment

--- models/reporting/googleads_campaign_performance.sql
+++ models/reporting/googleads_campaign_performance.sql
@@ -18,6 +18,6 @@ conversions_value as revenue,
 search_impression_share,
 search_budget_lost_impression_share,
 search_rank_lost_impression_share,
-signup as signups,
-consultationpayment as consultation_payment
+CASE WHEN date BETWEEN '2024-01-01' AND '2024-03-31' THEN appbariendocomwebregistersuccess ELSE signup END as signups,
+CASE WHEN date BETWEEN '2024-01-01' AND '2024-03-31' THEN reservationpayment ELSE consultationpayment END as consultation_payment
 FROM {{ref('googleads_performance_by_campaign')}}
