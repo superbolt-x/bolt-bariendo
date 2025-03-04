a319b3abd026c12afde76ca2654f3663c0d8c3ac
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-04 11:37:57 -0500

    Update posthog_signups_performance.sql

--- models/reporting/posthog_signups_performance.sql
+++ models/reporting/posthog_signups_performance.sql
@@ -1,1 +1,20 @@
+{{ config (
+    alias = target.database + '_posthog_signups_performance'
+)}}
 
+SELECT
+    CASE 
+        WHEN last_utm_source IN ('facebook','fb') THEN 'Meta' 
+        WHEN last_utm_source IN ('google','youtube') THEN 'Google'
+        WHEN last_utm_source IS NULL THEN 'Other'
+        ELSE 'Other' 
+    END AS channel,
+    first_signup_date,
+    first_utm_event_date,
+    first_utm_source,
+    first_utm_campaign,
+    last_utm_event_date,
+    last_utm_source,
+    last_utm_campaign
+FROM {{ source('s3_raw', 'signups') }}
+ORDER BY last_utm_event_date DESC
