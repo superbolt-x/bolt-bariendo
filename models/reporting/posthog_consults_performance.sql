3f8b2c50a22a20bc469f906b9e46c25eb1858787
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-11 14:35:49 -0400

    Update posthog_consults_performance.sql

--- models/reporting/posthog_consults_performance.sql
+++ models/reporting/posthog_consults_performance.sql
@@ -9,14 +9,14 @@ SELECT
         WHEN last_utm_source IS NULL THEN 'Other'
         ELSE 'Other' 
     END AS channel,
-    first_utm_event_date,
+    first_utm_event_date::date,
     first_utm_source,
     last_utm_source,
     first_utm_campaign,
-    last_utm_event_date,
+    last_utm_event_date::date,
     last_utm_campaign,
-    first_payment_date,
-    last_payment_date,
+    first_payment_date::date,
+    last_payment_date::date,
     hours_from_last_utm_event_to_payment
 FROM {{ source('s3_raw', 'consults') }}
 ORDER BY first_utm_event_date DESC
