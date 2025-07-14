05f4e82d7d07a7503b250383fbb867d6dbdf5972
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-07-14 16:16:45 -0600

    Update posthog_signups_performance.sql
    google_data - same as consults

--- models/reporting/posthog_signups_performance.sql
+++ models/reporting/posthog_signups_performance.sql
@@ -9,15 +9,29 @@ WITH initial_google_data as
     ),
 
     google_data as
-    (SELECT CASE WHEN last_utm_source IN ('google','youtube') THEN 'Google' ELSE 'Other' END AS channel,
-        first_signup_date::date, first_utm_event_date::date, first_utm_source, last_utm_source, g.first_utm_campaign, last_utm_event_date::date, g.last_utm_campaign
-    FROM initial_google_data
-    LEFT JOIN 
-        (SELECT count(*), campaign_id::varchar as first_campaign_id, campaign_id::varchar as last_campaign_id, campaign_name as first_utm_campaign, campaign_name as last_utm_campaign 
-        FROM {{ source('reporting', 'googleads_campaign_performance') }} 
-        GROUP BY 2,3,4,5) g
-    USING (first_campaign_id, last_campaign_id)
-    WHERE channel = 'Google'
+     (SELECT * FROM
+        (SELECT CASE WHEN last_utm_source IN ('google','youtube') THEN 'Google' ELSE 'Other' END AS channel,
+            first_signup_date::date, first_utm_event_date::date, first_utm_source, last_utm_source, g.first_utm_campaign, last_utm_event_date::date, 
+            NULL as last_utm_campaign
+        FROM initial_google_data
+        LEFT JOIN 
+            (SELECT count(*), campaign_id::varchar as first_campaign_id, campaign_name as first_utm_campaign
+            FROM {{ source('reporting', 'googleads_campaign_performance') }} 
+            GROUP BY 2,3) g
+        USING (first_campaign_id)
+        WHERE channel = 'Google'
+
+        UNION ALL
+
+        SELECT CASE WHEN last_utm_source IN ('google','youtube') THEN 'Google' ELSE 'Other' END AS channel,
+            first_signup_date::date, first_utm_event_date::date, first_utm_source, last_utm_source, g.first_utm_campaign, last_utm_event_date::date, g.last_utm_campaign
+        FROM initial_google_data
+        LEFT JOIN 
+            (SELECT count(*), campaign_id::varchar as last_campaign_id, campaign_name as last_utm_campaign 
+            FROM {{ source('reporting', 'googleads_campaign_performance') }} 
+            GROUP BY 2,3) g
+        USING (last_campaign_id)
+        WHERE channel = 'Google')
     ),
 
     other_data as
