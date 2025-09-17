78706cb2c3b517e0a80764df709aae0d8a4ae11b
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-09-17 15:39:04 +0200

    Update blended_campaign.sql

--- models/reporting/blended_campaign.sql
+++ models/reporting/blended_campaign.sql
@@ -4,46 +4,6 @@
 
 {%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
 
--- Macro to generate posthog data by granularity
-{% macro posthog_granular(source_table, date_col, metric) %}
-    {% for date_granularity in date_granularity_list %}
-    SELECT 
-        DATE_TRUNC('{{ date_granularity }}', {{ date_col }}) AS date,
-        '{{ date_granularity }}' AS date_granularity,
-        CASE
-            WHEN last_utm_campaign !~* 'gbp-listing' THEN 'Organic'
-            WHEN channel = 'Meta' THEN 'Meta'
-            WHEN channel = 'Google' THEN 'Google'
-            ELSE 'Other'
-        END AS channel,
-        CASE
-            WHEN channel = 'Meta' THEN 
-                COALESCE(
-                    fb_lookup.campaign_name,
-                    REPLACE(REPLACE(REPLACE(last_utm_campaign, '- Adv ', '- Adv+ '), '  ', ' '), 'Campaign Campaign', 'Campaign')
-                )
-            WHEN channel = 'Google' THEN 
-                COALESCE(g_lookup.campaign_name, last_utm_campaign)
-            ELSE last_utm_campaign
-        END AS campaign_name,
-        COUNT(*) AS {{ metric }}
-    FROM {{ source_table }} p
-    LEFT JOIN (
-        SELECT DISTINCT campaign_id::text, campaign_name
-        FROM reporting.bariendo_facebook_ad_performance
-    ) fb_lookup ON p.last_utm_campaign = fb_lookup.campaign_id::text
-        AND p.channel = 'Meta'
-    LEFT JOIN (
-        SELECT DISTINCT campaign_id::text, campaign_name
-        FROM reporting.bariendo_googleads_campaign_performance
-    ) g_lookup ON p.last_utm_campaign = g_lookup.campaign_id::text
-        AND p.channel = 'Google'
-    WHERE last_utm_campaign IS NOT NULL
-    GROUP BY 1,2,3,4
-    {% if not loop.last %} UNION ALL {% endif %}
-    {% endfor %}
-{% endmacro %}
-
 WITH spend_data AS (
     SELECT date, date_granularity, 'Google' AS channel, campaign_name, 
            MIN(campaign_id::text) AS campaign_id,
@@ -60,11 +20,11 @@ WITH spend_data AS (
 ),
 
 posthog_consults_data AS (
-    {{ posthog_granular('reporting.bariendo_posthog_consults_performance', 'last_payment_date', 'posthog_consults') }}
+    {{ posthog_granular_campaign('reporting.bariendo_posthog_consults_performance', 'last_payment_date', 'posthog_consults') }}
 ),
 
 posthog_signups_data AS (
-    {{ posthog_granular('reporting.bariendo_posthog_signups_performance', 'first_signup_date', 'posthog_signups') }}
+    {{ posthog_granular_campaign('reporting.bariendo_posthog_signups_performance', 'first_signup_date', 'posthog_signups') }}
 ),
 
 campaign_id_lookup AS (
