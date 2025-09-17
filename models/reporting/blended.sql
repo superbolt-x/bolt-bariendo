c7b6940bc447cd9bbc2f218065edd3cfab873ed9
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-09-17 15:53:08 +0200

    Update blended.sql

--- models/reporting/blended.sql
+++ models/reporting/blended.sql
@@ -5,51 +5,101 @@
 {%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
 
 WITH 
-    consults AS (
-        SELECT *, {{ get_date_parts('last_payment_date') }}
-        FROM {{ source('s3_raw', 'consults') }}
-    ),
-    signups AS (
-        SELECT *, {{ get_date_parts('first_signup_date') }}
-        FROM {{ source('s3_raw', 'signups') }}
-    ),
-    posthog_consults AS (
-        {{ posthog_granular('consults','last_payment_date','posthog_consults') }}
-    ),
-    posthog_signups AS (
-        {{ posthog_granular('signups','first_signup_date','posthog_signups') }}
-    ),
-    platform_data AS (
-        SELECT 
-            date, date_granularity, 'Google' AS channel,
-            SUM(spend) AS spend,
-            SUM(impressions) AS impressions,
-            SUM(clicks) AS clicks,
-            SUM(signups) AS signups,
-            SUM(consultation_payment) AS consults,
-            0 AS posthog_signups,
-            0 AS posthog_consults
-        FROM {{ source('reporting', 'googleads_campaign_performance') }}
-        GROUP BY 1,2,3
+-- Raw sources
+consults AS (
+    SELECT *, {{ get_date_parts('last_payment_date') }}
+    FROM {{ source('s3_raw', 'consults') }}
+),
+signups AS (
+    SELECT *, {{ get_date_parts('first_signup_date') }}
+    FROM {{ source('s3_raw', 'signups') }}
+),
 
-        UNION ALL
+-- Posthog consults
+posthog_consults AS (
+    {% for date_granularity in date_granularity_list %}
+    SELECT
+        DATE_TRUNC('{{ date_granularity }}', last_payment_date) AS date,
+        '{{ date_granularity }}' AS date_granularity,
+        CASE
+            WHEN last_utm_campaign !~* 'gbp-listing' THEN 'Organic'
+            WHEN last_utm_source IN ('facebook','fb') THEN 'Meta'
+            WHEN last_utm_source IN ('google','youtube') THEN 'Google'
+            ELSE 'Other'
+        END AS channel,
+        0 AS spend,
+        0 AS impressions,
+        0 AS clicks,
+        0 AS signups,
+        0 AS consults,
+        0 AS posthog_signups,
+        COUNT(*) AS posthog_consults
+    FROM consults
+    GROUP BY 1,2,3
+    {% if not loop.last %} UNION ALL {% endif %}
+    {% endfor %}
+),
 
-        SELECT 
-            date, date_granularity, 'Meta' AS channel,
-            SUM(spend), SUM(impressions), SUM(link_clicks), SUM(signups),
-            SUM(consultation_payment),
-            0,0
-        FROM {{ source('reporting', 'facebook_ad_performance') }}
-        GROUP BY 1,2,3
-    ),
-    combined_data AS (
-        SELECT * FROM platform_data
-        UNION ALL
-        SELECT * FROM posthog_signups
-        UNION ALL
-        SELECT * FROM posthog_consults
-    )
+-- Posthog signups
+posthog_signups AS (
+    {% for date_granularity in date_granularity_list %}
+    SELECT
+        DATE_TRUNC('{{ date_granularity }}', first_signup_date) AS date,
+        '{{ date_granularity }}' AS date_granularity,
+        CASE
+            WHEN last_utm_campaign !~* 'gbp-listing' THEN 'Organic'
+            WHEN last_utm_source IN ('facebook','fb') THEN 'Meta'
+            WHEN last_utm_source IN ('google','youtube') THEN 'Google'
+            ELSE 'Other'
+        END AS channel,
+        0 AS spend,
+        0 AS impressions,
+        0 AS clicks,
+        0 AS signups,
+        0 AS consults,
+        COUNT(*) AS posthog_signups,
+        0 AS posthog_consults
+    FROM signups
+    GROUP BY 1,2,3
+    {% if not loop.last %} UNION ALL {% endif %}
+    {% endfor %}
+),
 
+-- Paid platform data
+platform_data AS (
+    SELECT 
+        date, date_granularity, 'Google' AS channel,
+        SUM(spend) AS spend,
+        SUM(impressions) AS impressions,
+        SUM(clicks) AS clicks,
+        SUM(signups) AS signups,
+        SUM(consultation_payment) AS consults,
+        0 AS posthog_signups,
+        0 AS posthog_consults
+    FROM {{ source('reporting', 'googleads_campaign_performance') }}
+    GROUP BY 1,2,3
+
+    UNION ALL
+
+    SELECT 
+        date, date_granularity, 'Meta' AS channel,
+        SUM(spend), SUM(impressions), SUM(link_clicks), SUM(signups),
+        SUM(consultation_payment),
+        0,0
+    FROM {{ source('reporting', 'facebook_ad_performance') }}
+    GROUP BY 1,2,3
+),
+
+-- Combine all data (all have identical columns)
+combined_data AS (
+    SELECT * FROM platform_data
+    UNION ALL
+    SELECT * FROM posthog_signups
+    UNION ALL
+    SELECT * FROM posthog_consults
+)
+
+-- Final output
 SELECT
     date,
     date_granularity,
@@ -64,4 +114,4 @@ SELECT
 FROM combined_data
 WHERE date >= '2024-05-01'
 GROUP BY 1,2,3
-ORDER BY date DESC, date_granularity, channel
+ORDER BY date DESC, date_granularity, channel;
